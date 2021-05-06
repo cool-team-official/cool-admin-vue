259eb54d79a88e1f6180a7fd649f3504c653fcf4
jaredaddison <drjaredaddison.com>
2021-05-06 16:02:25 +0800

    One

--- src/cool/modules/chat/components/session.vue
+++ src/cool/modules/chat/components/session.vue
@@ -3,10 +3,10 @@
 		class="cl-chat-session"
 		:class="{
 			'is-position': browser.isMini,
+
 			'is-show': sessionVisible
 		}"
 	>
-		<!-- 关键字搜索 -->
 		<div class="cl-chat-session__search">
 			<el-input
 				v-model="keyWord"
@@ -18,8 +18,6 @@
 				@keyup.enter.native="onSearch"
 			></el-input>
 		</div>
-
-		<!-- 会话列表 -->
 		<ul class="cl-chat-session__list scroller1" v-loading="loading">
 			<li
 				class="cl-chat-session__item"
@@ -31,7 +29,6 @@
 				@click="toDetail(item)"
 				@contextmenu.stop.prevent="openCM($event, item.id, index)"
 			>
-				<!-- 头像 -->
 				<div class="avatar">
 					<el-badge
 						:value="item.serviceUnreadCount"
@@ -41,8 +38,6 @@
 						<img :src="item.headimgurl" alt="" />
 					</el-badge>
 				</div>
-
-				<!-- 昵称，内容 -->
 				<div class="det">
 					<p class="name">{{ item.nickname }}</p>
 					<p class="content">{{ item.lastMessage }}</p>
@@ -74,8 +69,6 @@ export default {
 
 	computed: {
 		...mapGetters(["sessionList", "session", "browser", "sessionVisible"]),
-
-		// 列表数据
 		list() {
 			return this.sessionList
 				.map(e => {
@@ -90,15 +83,11 @@ export default {
 	},
 
 	beforeCreate() {
-		// 销毁事件
 		eventBus.$off("session.refresh");
 	},
 
 	created() {
-		// 监听列表刷新
 		eventBus.$on("session.refresh", this.refresh);
-
-		// PC 端下首次请求读取第一个消息
 		this.refresh().then(res => {
 			if (!isEmpty(res.list) && !this.browser.isMini) {
 				this.SET_SESSION(res.list[0]);
@@ -108,8 +97,6 @@ export default {
 
 	methods: {
 		...mapMutations(["SET_SESSION_LIST", "SET_SESSION", "CLEAR_SESSION", "CLOSE_SESSION"]),
-
-		// 右键菜单
 		openCM(e, id, index) {
 			ContextMenu.open(e, {
 				list: [
@@ -133,8 +120,6 @@ export default {
 				]
 			});
 		},
-
-		// 刷新列表
 		refresh(params) {
 			this.loading = true;
 
@@ -162,19 +147,14 @@ export default {
 					});
 			});
 		},
-
-		// 搜索关键字
 		onSearch() {
-			this.refresh({ page: 1 });
+			this.refresh({
+				page: 1
+			});
 		},
-
-		// 会话详情
 		toDetail(item) {
 			if (item) {
-				// 点击关闭弹窗
 				if (this.browser.isMini) this.CLOSE_SESSION();
-
-				// 设置为当前会话
 				if (!this.session || this.session.id != item.id) {
 					this.SET_SESSION(item);
 				}
@@ -188,105 +168,107 @@ export default {
 
 <style lang="scss" scoped>
 .cl-chat-session {
-	height: 100%;
-	width: 0;
-	transition: width 0.2s ease-in-out;
-	border-radius: 5px;
-	background-color: #fff;
-	overflow: hidden;
+  overflow: hidden;
+  width: 0;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 5px;
+  transition: width .2s ease-in-out;
 
-	&.is-show {
-		width: 250px;
-		max-width: 100%;
-		margin-right: 5px;
-	}
+  &.is-show {
+    width: 250px;
+    max-width: 100%;
+    margin-right: 5px;
+  }
 
-	&.is-position {
-		position: absolute;
-		left: 5px;
-		top: 51px;
-		height: calc(100% - 56px);
-		z-index: 3000;
+  &.is-position {
+    position: absolute;
+    top: 51px;
+    left: 5px;
+    z-index: 3000;
+    height: calc(100% - 56px);
 
-		&.is-show {
-			width: calc(100% - 10px);
-		}
-	}
+    &.is-show {
+      width: calc(100% - 10px);
+    }
+  }
 
-	&__search {
-		padding: 10px;
-	}
+  &__search {
+    padding: 10px;
+  }
 
-	&__list {
-		height: calc(100% - 52px);
-		overflow: auto;
+  &__list {
+    overflow: auto;
+    height: calc(100% - 52px);
 
-		li {
-			display: flex;
-			list-style: none;
-			padding: 10px;
-			border-left: 5px solid #fff;
+    li {
+      display: flex;
+      padding: 10px;
+      list-style: none;
+      border-left: 5px solid #fff;
 
-			.avatar {
-				margin-right: 12px;
+      .avatar {
+        margin-right: 12px;
 
-				img {
-					display: block;
-					height: 40px;
-					width: 40px;
-					border-radius: 3px;
-					background-color: #eee;
-				}
+        img {
+          display: block;
+          width: 40px;
+          height: 40px;
+          background-color: #eee;
+          border-radius: 3px;
+        }
 
-				.el-badge {
-					&__content {
-						height: 14px;
-						line-height: 14px;
-						padding: 0 4px;
-						background-color: #fa5151;
-						border: 0;
-					}
-				}
-			}
+        .el-badge {
+          &__content {
+            height: 14px;
+            padding: 0 4px;
+            line-height: 14px;
+            background-color: #fa5151;
+            border: 0;
+          }
+        }
+      }
 
-			.det {
-				flex: 1;
-				.name {
-					font-size: 13px;
-					margin-top: 1px;
-				}
+      .det {
+        flex: 1;
 
-				.content {
-					font-size: 12px;
-					margin-top: 5px;
-					color: #666;
-				}
+        .name {
+          margin-top: 1px;
+          font-size: 13px;
+        }
 
-				.name,
-				.content {
-					overflow: hidden;
-					text-overflow: ellipsis;
-					display: -webkit-box;
-					-webkit-box-orient: vertical;
-					-webkit-line-clamp: 1;
-				}
-			}
+        .content {
+          margin-top: 5px;
+          color: #666;
+          font-size: 12px;
+        }
 
-			&.is-active {
-				background-color: #eee;
-				border-color: $color-primary;
-			}
+        .name,
+        .content {
+          display: -webkit-box;
+          overflow: hidden;
+          text-overflow: ellipsis;
 
-			&:hover {
-				background-color: #eee;
-				cursor: pointer;
-			}
-		}
-	}
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 1;
+        }
+      }
 
-	&__empty {
-		text-align: center;
-		margin-top: 10px;
-	}
+      &.is-active {
+        background-color: #eee;
+        border-color: $color-primary;
+      }
+
+      &:hover {
+        background-color: #eee;
+        cursor: pointer;
+      }
+    }
+  }
+
+  &__empty {
+    margin-top: 10px;
+    text-align: center;
+  }
 }
 </style>
