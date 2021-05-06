8ca5f8ecebd133926bd17e1250c33fef4a3fe216
jaredaddison <drjaredaddison.com>
2021-05-06 16:04:11 +0800

    2

--- src/config/env.js
+++ src/config/env.js
@@ -21,25 +21,22 @@ export const baseUrl = (function() {
     return isDev ? `/${proxy}/admin` : `/api/admin`;
 })();
 
-// 阿里字体图标库 https://at.alicdn.com/t/**.css
 export const iconfontUrl = ``;
 
-// 程序配置参数
 export const app = store.get("__app__") || {
     name: "COOL-ADMIN",
 
     conf: {
-        showAMenu: false, // 是否显示一级菜单栏
-        showRouteNav: true, // 是否显示路由导航栏
-        showProcess: true, // 是否显示页面进程栏
-        customMenu: false // 自定义菜单
+        showAMenu: false,
+        showRouteNav: true,
+        showProcess: true,
+        customMenu: false
     },
 
     theme: {
-        color: "", // 主题色
-        url: "" // 主题样式地址
+        color: "",
+        url: ""
     }
 };
 
-// 自定义菜单列表
 export const menuList = [];
