bc30b0d891192ef679d77b4c1670f30c9002fa15
icssoa <615206459@qq.com>
2022-05-16 14:02:44 +0800

    添加组件示例

---
+++ src/modules/demo/components/demo/svg.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="scope">
+		<div class="h">
+			<span>icon-svg</span>
+			svg图片库
+		</div>
+		<div class="c _svg">
+			<el-tooltip v-for="(item, index) in list" :key="index" content="icon-like">
+				<icon-svg :size="18" :name="`icon-${item}`" />
+			</el-tooltip>
+		</div>
+		<div class="f">
+			<span class="date">2019/09/25</span>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const list = ref(["like", "video", "rank", "menu", "favor"]);
+</script>
