c63d37456508a9628e4453d18aede1671d5d3512
icssoa <615206459@qq.com>
2022-05-19 16:17:56 +0800

    添加分割打包

--- vite.config.ts
+++ vite.config.ts
@@ -53,7 +53,16 @@ export default (): UserConfig => {
 		},
 		build: {
 			sourcemap: false,
-			polyfillDynamicImport: false // 必须为false
+			polyfillDynamicImport: false, // 必须为false
+			rollupOptions: {
+				output: {
+					manualChunks(id) {
+						if (id.includes("node_modules")) {
+							return id.toString().split("node_modules/")[1].split("/")[0].toString();
+						}
+					}
+				}
+			}
 		}
 	};
 };
