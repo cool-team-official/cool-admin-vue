e2e573844b663c129cf0278d17bca227f5cfc233
神仙都没用 <615206459@qq.com>
2023-10-07 10:15:53 +0800

    优化

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -1,7 +1,6 @@
 export const proxy = {
 	"/dev/": {
-		// target: "http://127.0.0.1:8001",
-		target: "https://admin-dev.cool-js.cloud",
+		target: "http://127.0.0.1:8001",
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, "")
 	},
