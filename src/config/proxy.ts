9afad4d475364ab74ade3856a8ddfe3f035ae665
神仙都没用 <615206459@qq.com>
2024-10-18 22:48:58 +0800

    优化

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -1,6 +1,6 @@
 export const proxy = {
 	'/dev/': {
-		target: 'http://127.0.0.1:8888',
+		target: 'http://127.0.0.1:8001',
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, '')
 	},
